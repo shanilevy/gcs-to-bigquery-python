110909083d6d70361abdc7e748d23dd4cd00821b
Shani Levy <shanilevy@google.com>
2021-06-30 15:45:30 +1000

    test

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
-#!/usr/bin/python
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-import time
-from flask import Flask
-app = Flask(__name__)
+project_id = "mhanline-sand07"
+key_path = "credentials.json"
 
-START = time.time()
+credentials = service_account.Credentials.from_service_account_file(
+    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+)
 
-def elapsed():
-    running = time.time() - START
-    minutes, seconds = divmod(running, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%d:%02d:%02d" % (hours, minutes, seconds)
+client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
 
-@app.route('/')
-def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
+# Construct a BigQuery client object.
+#client = bigquery.Client()
+    
+# TODO(developer): Set table_id to the ID of the table to create.
+table_id = "mhanline-sand07.dwh.wikipedia_pageviews_2021"
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("datehour", "TIMESTAMP"),
+        bigquery.SchemaField("wiki", "STRING"),
+        bigquery.SchemaField("title", "STRING"),
+        bigquery.SchemaField("views", "INTEGER"),
+    ],
+    skip_leading_rows=1,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = "gs://my-dataops-bucket-123/wikipedia_pageviews_2021-000000000000.csv"
+
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
+# [END bigquery_load_table_gcs_csv]
