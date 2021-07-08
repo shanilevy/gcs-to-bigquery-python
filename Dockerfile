38d3890f875672eebb8f72dbe38ae454a07f0b59
Shani Levy <shanilevy@google.com>
2021-07-08 15:03:27 +1000

    test

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,24 @@
-#FROM python:3-alpine
-#ADD app.py /
-#COPY requirements.txt /
-#RUN pip install -r requirements.txt
-#ENTRYPOINT ["python3", "./app.py"]
-FROM python:3.8-slim-buster
+FROM python:3.9-slim
 
 RUN python3 -m venv /opt/venv
 
-# Install dependencies:
-COPY requirements.txt .
+# Allow statements and log messages to immediately appear in the Knative logs
+ENV PYTHONUNBUFFERED True
+
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+# Install production dependencies.
 RUN . /opt/venv/bin/activate && pip install -r requirements.txt
 
-# Run the application:
-COPY app.py .
-#COPY credentials.json .
-#CMD . /opt/venv/bin/activate && exec python app.py
-CMD  exec gunicorn --bind :$PORT app:app
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+CMD . /opt/venv/bin/activate && exec gunicorn --bind :8080 --workers 1 --threads 8 --timeout 0 main:app
+
+# [END run_helloworld_dockerfile]
+# [END cloudrun_helloworld_dockerfile]
--- main.py
+++ main.py
@@ -1,22 +1,43 @@
+# Copyright 2020 Google, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START cloudrun_helloworld_service]
+# [START run_helloworld_service]
+import os
+
+from flask import Flask
+
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
 project_id = "dataops-319100"
 
 app = Flask(__name__)
+
 #key_path = "credentials.json"
 
 #credentials = service_account.Credentials.from_service_account_file(
 #    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
 #)
 
-#client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
+#client=bigquery.Client(credentials=credentials, project=credentials.project_id,)
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
     
 # TODO(developer): Set table_id to the ID of the table to create.
-table_id = "dataops-319100.dwh.wikipedia_pageviews_2021"
+table_id="dataops-319100.dwh.wikipedia_pageviews_2021"
 
 job_config = bigquery.LoadJobConfig(
     schema=[
@@ -29,7 +50,7 @@ job_config = bigquery.LoadJobConfig(
     # The source format defaults to CSV, so the line below is optional.
     source_format=bigquery.SourceFormat.CSV,
 )
-uri = "gs://tmer-dataops-bucket-123/wikipedia_pageviews_2021-000000000000.csv"
+uri = "gs://tmer-dataops-bucket-123/wikipedia_pageviews_2021-000000000001.csv"
 
 load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
@@ -44,9 +65,9 @@ print("Loaded {} rows.".format(destination_table.num_rows))
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
-    
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0',
-            port=int(os.environ.get(
-                     'PORT', 8080)))
-# [END bigquery_load_table_gcs_csv]
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# [END run_helloworld_service]
+# [END cloudrun_helloworld_service]
