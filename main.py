b74cf0c324dd687efc09f0bf91ede3127e3aab0c
Shani Levy <shanilevy@google.com>
2021-07-13 10:27:59 +1000

    test

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def index():
     #url = name["SelfLink"]
     #resp2 = f"this is my link: {url}"
     print(resp)
-    print("TESTING: ", pubsub_message['name'])
+    print("TESTING: ", pubsub_message['data']['name'])
 
     load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
