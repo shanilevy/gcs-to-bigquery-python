2f6f6212d84d5d37073cc6d9925cbc9839276823
Shani Levy <shanilevy@google.com>
2021-07-19 10:16:51 +1000

    test

--- main.py
+++ main.py
@@ -88,21 +88,21 @@ uri = "gs://tmer-dataops-bucket-123/wikipedia_pageviews_2021-000000000006.csv"
 # [START eventarc_pubsub_handler]
 @app.route('/', methods=['POST'])
 def index():
-    #subscriber = pubsub_v1.SubscriberClient()
+    subscriber = pubsub_v1.SubscriberClient()
 
     # existing subscription
-    #subscription_path = subscriber.subscription_path(
-    #    project_id, subscirption_id)
+    subscription_path = subscriber.subscription_path(
+        project_id, subscirption_id)
 
-    #response = subscriber.pull(
-    #    request={
-    #        "subscription": subscription_path,
-    #        "max_messages": MAX_MESSAGES,
-    #    }
-    #)
+    response = subscriber.pull(
+        request={
+            "subscription": subscription_path,
+            "max_messages": MAX_MESSAGES,
+        }
+    )
 
-    #for msg in response.received_messages:
-    #    print("Received message:", msg.message.data)
+    for msg in response.received_messages:
+        print("Received message:", msg.message.data)
 
     data = request.get_json()
     if not data:
@@ -141,13 +141,13 @@ def index():
             destination_table = client.get_table(table_id)  # Make an API request.
             print("Loaded {} rows.".format(destination_table.num_rows))
             
-            #ack_ids = [msg.ack_id for msg in response.received_messages]
-            #subscriber.acknowledge(
-            #    request={
-            #        "subscription": subscription_path,
-            #        "ack_ids": ack_ids,
-            #    }
-            #)
+            ack_ids = [msg.ack_id for msg in response.received_messages]
+            subscriber.acknowledge(
+                request={
+                    "subscription": subscription_path,
+                    "ack_ids": ack_ids,
+                }
+            )
 
             return (resp, 200)
         else:
