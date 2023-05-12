5dac4fd8fbc77c3126532e359dd15fec821496f0
shanilevy <shani.levy77@gmail.com>
2023-05-12 13:16:39 +1000

    test

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ version = "1.0"
 client = bigquery.Client()
     
 # TODO(developer): Set table_id to the ID of the table to create.
-table_id="{project}.dwh.wikipedia_pageviews_2021"
+table_id="dataops-terraform-example.dwh.wikipedia_pageviews_2021"
 
 job_config = bigquery.LoadJobConfig(
     schema=[
@@ -100,7 +100,7 @@ def index():
             #before loading clear the staging table
             query_job = client.query(
                 """
-                TRUNCATE TABLE `{project}.dwh.wikipedia_pageviews_2021`
+                TRUNCATE TABLE `dataops-terraform-example.dwh.wikipedia_pageviews_2021`
                 """
             )
 
